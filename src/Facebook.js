20991205da4605ad4e745d3db8cec22f40c340d9
dalim-kazi <mddalim862483@gmail.com>
2023-12-05 00:45:48 +0600

    first commit

---
+++ src/Facebook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+const Facebook = () => {
+    return (
+        <>
+    <MessengerCustomerChat
+    pageId="237614619344644"
+    appId="100973205345215"
+  />,
+        </>
+    );
+};
+
+export default Facebook;
